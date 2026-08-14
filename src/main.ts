8209fec87919713a38effec2bef619b42b01b73f
l1pc24 <l1pc24@estudiantes.anai.lan>
2026-08-14 08:22:11 -0500

    CORRECION DE CROS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 👇 Esta es la llave mágica que permite la conexión
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
