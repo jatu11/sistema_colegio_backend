bfed06f253554b22b9500ed1081e436fef029fb3
l1pc24 <l1pc24@estudiantes.anai.lan>
2026-08-19 17:42:52 -0500

    feat: implementación de arquitectura PBAC (Cargos y Permisos), endpoints semilla y relación con usuarios2

--- src/permisos/entities/permiso.entity.ts
+++ src/permisos/entities/permiso.entity.ts
@@ -3,11 +3,11 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity('permisos')
 export class Permiso {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column({ unique: true })
-  nombre: string; // Ej: 'matricular:estudiantes'
+  nombre!: string; // Ej: 'matricular:estudiantes'
 
   @Column({ nullable: true })
-  descripcion: string; // Ej: 'Permite matricular cadetes en el periodo actual'
+  descripcion!: string; // Ej: 'Permite matricular cadetes en el periodo actual'
 }
